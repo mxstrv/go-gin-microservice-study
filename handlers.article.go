feb80949c2845b126117951dd6c9f80568897c9e
mxstrv <37843972+mxstrv@users.noreply.github.com>
2023-07-06 22:55:22 +0300

    modify showindexpage function

--- handlers.article.go
+++ handlers.article.go
@@ -9,12 +9,12 @@ import (
 func showIndexPage(c *gin.Context) {
 	articles := getAllArticles()
 
-	c.HTML(http.StatusOK, "index.html",
-		gin.H{
-			"title":   "Home Page",
-			"payload": articles,
-		})
+	// Call the render function
+	render(c, gin.H{
+		"title":   "Home Page",
+		"payload": articles}, "index.html")
 }
+
 func getArticle(c *gin.Context) {
 	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
 		if article, err := getArticleByID(articleID); err == nil {
